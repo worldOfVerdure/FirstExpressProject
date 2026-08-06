10b12aa4f4ae24042ee8c3053be24ff59761850d
worldOfVerdure <contact@andrewchupka.com>
2026-08-06 16:56:44 -0400

    feat: shop.ts beginning dynamic code

--- routes/shop.ts
+++ routes/shop.ts
@@ -2,11 +2,12 @@ import express from 'express';
 //data
 import * as adminData from './admin.ts';
 //utility function
-import { getSafePath } from '../util/path.ts';
+// import { getSafePath } from '../util/path.ts';
 
 export const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  const products = adminData.products;
   res.render('shop');
   // console.log(adminData.products);
   // const filePath = getSafePath('views/shop.html');
